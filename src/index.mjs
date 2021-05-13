883a4c20531d9e9cfef4d9e5c35991be4bdbf6fa
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-05-13 11:49:16 -0300

    feat: :construction: initial k8s setup with pods and replicaset
    initial commit with k8s setup with pods and replicaset

---
+++ src/index.mjs
@@ -0,0 +1,17 @@
+import http from 'http';
+
+const SERVER_PORT = 4000;
+
+const server = http.createServer((_, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  res.statusCode = 200;
+
+  res.end(JSON.stringify({
+    alive: true
+  }));
+});
+
+server.listen(SERVER_PORT, () => {
+  console.log(`Server running at ${SERVER_PORT} port`);
+});
