b5beb2349d4b479dcddc8696117413ca20a4354f
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-05-14 19:15:30 -0300

    feat: added configmap
    added configmap with environment variables

--- src/index.mjs
+++ src/index.mjs
@@ -3,12 +3,18 @@ import http from 'http';
 const SERVER_PORT = 4000;
 
 const server = http.createServer((_, res) => {
+  let message = 'nodejs_k8s';
+
+  if (process.env.NAME && process.env.AGE) {
+    message = message.concat(`- I'm ${process.env.NAME}. I'm ${process.env.AGE} years old.`);
+  }
+
   res.setHeader('Content-Type', 'application/json');
 
   res.statusCode = 200;
 
   res.end(JSON.stringify({
-    message: 'nodejs_k8s',
+    message,
     alive: true
   }));
 });
