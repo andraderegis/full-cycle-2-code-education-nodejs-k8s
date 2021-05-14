1675defa95fb54e26c001cbf5e03be72bf11e56b
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-05-14 20:19:29 -0300

    feat: added secrets

--- src/index.mjs
+++ src/index.mjs
@@ -4,9 +4,17 @@ const SERVER_PORT = 4000;
 
 const server = http.createServer((_, res) => {
   let message = 'nodejs_k8s';
+  let user = {};
 
   if (process.env.NAME && process.env.AGE) {
     message = message.concat(`- I'm ${process.env.NAME}. I'm ${process.env.AGE} years old.`);
+  }
+
+  if (process.env.USER && process.env.EMAIL) {
+    user = {
+      username: process.env.USER,
+      email: process.env.EMAIL
+    }
   }
 
   res.setHeader('Content-Type', 'application/json');
@@ -14,6 +22,7 @@ const server = http.createServer((_, res) => {
   res.statusCode = 200;
 
   res.end(JSON.stringify({
+    user,
     message,
     alive: true
   }));
